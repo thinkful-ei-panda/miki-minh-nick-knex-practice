74d23dda75ab062ddd8ab9826be0b24b9a3745ac
nickthorpe71 <nickthorpe71@gmail.com>
2020-07-14 15:24:08 -0500

    --

--- src/drills.js
+++ src/drills.js
@@ -2,29 +2,29 @@ require('dotenv').config();
 const knex = require('knex');
 
 const db = knex({
-    client: 'pg',
-    connection: process.env.DB_URL
+  client: 'pg',
+  connection: process.env.DB_URL
 });
 
 function getItemsWithText(searchTerm) {
-    db('shopping_list')
-        .select('*')
-        .where('item_name', 'ilike', `%${searchTerm}%`)
-        .then(results => console.log(results))
-        .finally(() => db.destroy())
+  db('shopping_list')
+    .select('*')
+    .where('item_name', 'ilike', `%${searchTerm}%`)
+    .then(results => console.log(results))
+    .finally(() => db.destroy())
 };
 
 // getItemsWithText('fish')
 
 function getAllItemsPaginated(pageNumber, limit) {
-    const offsetNumber = (pageNumber-1) * limit
+  const offsetNumber = (pageNumber - 1) * limit
 
-    db('shopping_list')
-        .select('*')
-        .limit(`${limit}`)
-        .offset(`${offsetNumber}`)
-        .then(results => console.log(results))
-        .finally(() => db.destroy())
+  db('shopping_list')
+    .select('*')
+    .limit(`${limit}`)
+    .offset(`${offsetNumber}`)
+    .then(results => console.log(results))
+    .finally(() => db.destroy())
 };
 
 getAllItemsPaginated(2, 31)
