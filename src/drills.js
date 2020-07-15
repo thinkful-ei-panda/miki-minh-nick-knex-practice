37f115252b0669dc9e7ef12449d2b0a8b6aa4e87
mischkiey <mischkieycf@gmail.com>
2020-07-15 06:50:05 -0400

    Changed item_name to category in getTotalPriceByCategory

--- src/drills.js
+++ src/drills.js
@@ -2,54 +2,54 @@ require('dotenv').config();
 const knex = require('knex');
 
 const db = knex({
-    client: 'pg',
-    connection: process.env.DB_URL
+  client: 'pg',
+  connection: process.env.DB_URL
 });
 
 function getItemsWithText(searchTerm) {
-    db('shopping_list')
-        .select('*')
-        .where('item_name', 'ilike', `%${searchTerm}%`)
-        .then(results => console.log(results))
-        .finally(() => db.destroy())
-};
+  db('shopping_list')
+    .select('*')
+    .where('item_name', 'ilike', `%${searchTerm}%`)
+    .then(results => console.log(results))
+    .finally(() => db.destroy());
+}
 
 // getItemsWithText('fish')
 
 function getAllItemsPaginated(pageNumber, limit) {
-    const offsetNumber = (pageNumber-1) * limit
+  const offsetNumber = (pageNumber-1) * limit;
 
-    db('shopping_list')
-        .select('*')
-        .limit(`${limit}`)
-        .offset(`${offsetNumber}`)
-        .then(results => console.log(results))
-        .finally(() => db.destroy())
-};
+  db('shopping_list')
+    .select('*')
+    .limit(`${limit}`)
+    .offset(`${offsetNumber}`)
+    .then(results => console.log(results))
+    .finally(() => db.destroy());
+}
 
 // getAllItemsPaginated(2, 31)
 
 function getRowsNewerThan(daysAgo) {
-    db('shopping_list')
-        .select('*')
-        .where('date_added',
-            '>',
-            db.raw(`now() -'?? days'::INTERVAL`, daysAgo)
-        )
-        .then(res => console.log(res))
-        // .catch(err => console.warning(err.message))
-        .finally(() => db.destroy());
+  db('shopping_list')
+    .select('*')
+    .where('date_added',
+      '>',
+      db.raw(`now() -'?? days'::INTERVAL`, daysAgo)
+    )
+    .then(res => console.log(res))
+  // .catch(err => console.warning(err.message))
+    .finally(() => db.destroy());
 }
 
 // getRowsNewerThan(14);
 
 function getTotalPriceByCategory() {
-    db('shopping_list')
-        .select('category')
-        .sum('price AS total_price')
-        .groupBy('category')
-        .then(res => console.log(res))
-        .finally(() => db.destroy());
+  db('shopping_list')
+    .select('category')
+    .sum('price AS total_price')
+    .groupBy('category')
+    .then(res => console.log(res))
+    .finally(() => db.destroy());
 }
 
-getTotalPriceByCategory(); 
+getTotalPriceByCategory();
